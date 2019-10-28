3ae61c1223fa59c8f0ddc35398ee68da29150541
HuuNhuan <huunhuan2002@gmail.com>
2019-10-28 21:02:52 +0700

    update code

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from flask_restful import Api
-from controller import Authenticate
+from controller import Authenticate, Webhook
 
 app = Flask(__name__)
 api = Api(app)
 
 api.add_resource(Authenticate.Authenticate, '/auth')
+api.add_resource(Webhook.WebHook, '/webhook')
 
 if __name__ == '__main__':
     app.run(debug=True)
